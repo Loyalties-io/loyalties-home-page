72b2afa8504ac2eab9fb668e6625bb32af5491e8
cato b <cato.benjaminsen@gmail.com>
2024-06-05 15:53:09 +0200

    Add company details and stick footer to bottom on imprint page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,41 +2,39 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Footer.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const Footer = () => (
-  <Navbar expand="lg" className="footer">
-    {/* add fixed="bottom" and add margin to the bottom of each page */}
-    <Container>
-      {/* <Navbar.Brand as={Link} to="/">
-        <img
-          src="https://test-public-bucket-loy.s3.eu-central-1.amazonaws.com/logo_large_black.png"
-          width="115"
-          className="d-inline-block align-top"
-          alt="Loyalties logo"
-        />
-      </Navbar.Brand> */}
-      <Nav className="footer-content">
-        <div className="flex-wrapper">
-          <div className="links-wrapper">
-            <Nav.Link as={Link} to="/imprint" className="footer-link">
-              Imprint
-            </Nav.Link>
-            <Nav.Link as={Link} to="/terms" className="footer-link">
-              Terms & Conditions
-            </Nav.Link>
-            <Nav.Link as={Link} to="/privacy" className="footer-link">
-              Privacy Policy
-            </Nav.Link>
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <Navbar
+      expand="lg"
+      fixed={pathname === "/imprint" ? "bottom" : undefined}
+      className="footer"
+    >
+      <Container>
+        <Nav className="footer-content">
+          <div className="flex-wrapper">
+            <div className="links-wrapper">
+              <Nav.Link as={Link} to="/imprint" className="footer-link">
+                Imprint
+              </Nav.Link>
+              <Nav.Link as={Link} to="/terms" className="footer-link">
+                Terms & Conditions
+              </Nav.Link>
+              <Nav.Link as={Link} to="/privacy" className="footer-link">
+                Privacy Policy
+              </Nav.Link>
+            </div>
+            <div>
+              <p className="copyright">2024 Loyalties</p>
+            </div>
           </div>
-          <div>
-            <p className="copyright">2024 Loyalties</p>
-          </div>
-        </div>
-      </Nav>
-      {/* <Nav></Nav> */}
-    </Container>
-  </Navbar>
-);
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Footer;
--- src/pages/Imprint.jsx
+++ src/pages/Imprint.jsx
@@ -8,8 +8,11 @@ const Imprint = () => (
       <h1 className="page-heading">Imprint</h1>
       <div className="page-text-container">
         <div className="page-section">
-          <p className="page-section-heading">Address:</p>
-          <p>Address here</p>
+          <p className="page-section-heading">mmmh AG</p>
+          <p>Fledhof 12. 6300 Zug, Switzerland</p>
+          <p>CHE-201.571.803 MWST</p>
+          <p>Email: contact@mmmh.studo</p>
+          <p>Phone: 1234567890</p>
         </div>
       </div>
     </Row>
