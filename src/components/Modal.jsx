d771122fafc29c99ec0ffac2335f7dbab8ac9a97
cato b <cato.benjaminsen@gmail.com>
2024-08-07 00:29:38 +0200

    Add google play store icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,13 +11,22 @@ const modal = (props) => {
     >
       <Modal.Body className="modal-content">
         <h2>Join the community!</h2>
-        <a href="https://apps.apple.com/us/app/loyalties-io/id6587571478">
-          <img
-            src="https://test-public-bucket-loy.s3.eu-central-1.amazonaws.com/app-store.png"
-            alt="app store"
-            width="140"
-          />
-        </a>
+        <div className="download-wrapper">
+          <a href="https://apps.apple.com/us/app/loyalties-io/id6587571478">
+            <img
+              src="https://test-public-bucket-loy.s3.eu-central-1.amazonaws.com/app-store.png"
+              alt="apple app store icon"
+              width="140"
+            />
+          </a>
+          <a href="hhttps://play.google.com/store/apps/details?id=com.cato.benjaminsen.loyaltyfrontend">
+            <img
+              src="https://test-public-bucket-loy.s3.eu-central-1.amazonaws.com/google-play-store-icon.svg"
+              alt="google play store icon"
+              width="160"
+            />
+          </a>
+        </div>
         <p>
           Are you a business looking to use Loyalties? <br />
           Email us{" "}
