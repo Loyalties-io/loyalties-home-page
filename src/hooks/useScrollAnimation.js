6d3c18def1a7942e0291c509fd19da2a6bc147a8
cato b <cato.benjaminsen@gmail.com>
2025-11-14 11:22:21 +0100

    Fixes

--- src/hooks/useScrollAnimation.js
+++ src/hooks/useScrollAnimation.js
@@ -22,13 +22,14 @@ export const useScrollAnimation = (options = {}) => {
       }
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, [options.threshold, options.rootMargin, options.once]);
